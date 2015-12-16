1d861a5dfc9192c61005d79d4152d6b035cbe945
Peter Wendorff <jongleur1983@gmx.de>
2015-12-16 20:14:31 +0100

    add unit test project for GapText control to XamlLocalizationTest.sln

---
+++ GapTextWpfTest/Converters/ObjectTypeEqualsConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GapTextWpfTest.Converters
+{
+    public class ObjectTypeEqualsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // returns true if and only if value is of type parameter
+            var comparedType = parameter as Type;
+            return comparedType != null && comparedType.IsInstanceOfType(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ GapTextWpfTest/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GapTextWpfTest
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> WeekDays { get; set; }
+
+        public DateTime OpeningTime { get; set; }
+
+        public DateTime ClosingTime { get; set; }
+
+        public string City { get; set; }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.City = "Paderborn";
+            this.OpeningTime = new DateTime(1, 1, 1, 09, 30, 00);
+            this.ClosingTime = new DateTime(1, 1, 1, 16, 0, 0);
+        }
+    }
+}
