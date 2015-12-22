2c8ab2b718409c3f4337fee85935c33a4647fd09
Peter Wendorff <jongleur1983@gmx.de>
2015-12-22 23:46:32 +0100

    add a demo project for the current state identified restriction: works with explicit relativeSource binding with anchor element GapTextControl, but not with the short syntax. To be continued.

---
+++ WPFLocalizationExtensionDemoApplication/Infrastructure/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using WPFLocalizationExtensionDemoApplication.ViewModels;
+
+namespace WPFLocalizationExtensionDemoApplication.Infrastructure
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ WPFLocalizationExtensionDemoApplication/ViewModels/Examples/TextLocalizationExampleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace WPFLocalizationExtensionDemoApplication.ViewModels.Examples
+{
+    public class TextLocalizationExampleViewModel : Screen
+    {
+        public TextLocalizationExampleViewModel()
+        {
+            base.DisplayName = "TextLocalizationExample";
+        }
+    }
+}
