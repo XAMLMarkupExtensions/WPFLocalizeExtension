307b47cd4a1cfcbe63ea7b8de656e167008add9f
SeriousM <bernhard.millauer@gmail.com>
2018-01-08 21:32:00 +0100

    fixup build process

--- build/push.bat
+++ build/push.bat
@@ -3,11 +3,11 @@
 set PWD="%CD%"
 set NUGET=%PWD%\NuGet.exe
 
-set /P APIKEY=Enter your Api Key from https://www.nuget.org/account: 
+set /P APIKEY=Enter your Api Key from https://www.nuget.org/account/ApiKeys: 
 
 if "%APIKEY%"=="" GOTO ERROR
 
-%NUGET% push %PWD%\..\*.nupkg %APIKEY%
+%NUGET% push %PWD%\..\*.nupkg %APIKEY% -Source https://www.nuget.org/api/v2/package
 GOTO END
 
 :ERROR
