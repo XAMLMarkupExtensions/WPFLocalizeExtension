fe7a079e1ed1cfcaf1cc8c6b4c173ac58f70d54f
Konrad Mattheis <konrad.mattheis@analytics2go.net>
2019-02-23 14:48:05 +0100

    fix MemoryTest

--- tests/AssemblyTestResourceLib/Properties/AssemblyInfo.cs
+++ tests/AssemblyTestResourceLib/Properties/AssemblyInfo.cs
@@ -2,7 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
