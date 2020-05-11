cfbad7d52476657b3191d3069aa196acd22a513e
Konrad Mattheis <konrad@mattheis.name>
2020-05-11 16:24:05 +0200

    add LeakSample

---
+++ tests/LeakSample/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace LeakSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnReload(object sender, RoutedEventArgs e)
+        {
+            ItemsControl.ItemsSource = new object[new Random().Next(10, 30)];
+        }
+    }
+}
---
+++ tests/LeakSample/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using LeakSample.Annotations;
+
+namespace LeakSample
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public IReadOnlyList<object> Objects { get; private set; }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
