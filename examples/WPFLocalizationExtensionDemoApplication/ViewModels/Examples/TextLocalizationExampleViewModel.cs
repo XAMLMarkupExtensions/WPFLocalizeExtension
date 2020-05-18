85d739b246d6ca9602c46db8e35b7154e5cd35cc
sa-he <samuel.hess@gmx.de>
2020-05-18 08:33:25 +0200

    Fix INPC-event in demo VM

--- examples/WPFLocalizationExtensionDemoApplication/ViewModels/Examples/TextLocalizationExampleViewModel.cs
+++ examples/WPFLocalizationExtensionDemoApplication/ViewModels/Examples/TextLocalizationExampleViewModel.cs
@@ -1,28 +1,65 @@
 ﻿using Caliburn.Micro;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using WPFLocalizeExtension;
+using WPFLocalizeExtension.Extensions;
+using WPFLocalizeExtension.Providers;
 
 namespace WPFLocalizationExtensionDemoApplication.ViewModels.Examples
 {
-    public class TextLocalizationExampleViewModel : Screen
+    public class TextLocalizationExampleViewModel : Screen, INotifyPropertyChanged
     {
         private string _resourceKey;
+
+        private string _resourceText;
 
         public TextLocalizationExampleViewModel()
         {
             base.DisplayName = ResourceKey = "TextLocalizationExample";
+
+            BindPropertyToResource(nameof(ResourceText), "TextLocalizationExample2");
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public string ResourceKey
         {
             get { return _resourceKey; }
             set
             {
-                if (value == _resourceKey) return;
+                if (value == _resourceKey)
+                    return;
                 _resourceKey = value;
-                NotifyOfPropertyChange(() => ResourceKey);
+                OnPropertyChanged();
             }
         }
 
         public ResourceKeyViewModel ResourceKeyVM { get; set; } = new ResourceKeyViewModel("TextLocalizationExample");
+
+        public string ResourceText
+        {
+            get { return _resourceText; }
+            set
+            {
+                if (value == _resourceText) return;
+                _resourceText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void BindPropertyToResource(string propertyName, string resourceKey)
+        {
+            var resxLocalizationProvider = ResxLocalizationProvider.Instance;
+
+            var targetProperty = GetType().GetProperty(propertyName);
+            var locBinding = new LocTextExtension(resourceKey);
+
+            locBinding.SetBinding(this, targetProperty);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
