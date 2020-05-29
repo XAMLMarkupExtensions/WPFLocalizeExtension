16050abbeb670ee57dd6e48d0e84512ffeffb300
Konrad Mattheis <konrad@mattheis.name>
2020-05-29 12:41:40 +0200

    add missing key to bloc

--- src/Extensions/BLoc.cs
+++ src/Extensions/BLoc.cs
@@ -290,7 +290,7 @@ namespace WPFLocalizeExtension.Extensions
         /// </summary>
         public object FormatOutput()
         {
-            object result;
+            object result = null;
 
             // Try to get the localized input from the resource.
             string resourceKey = LocalizeDictionary.Instance.GetFullyQualifiedResourceKey(Key, null);
@@ -304,13 +304,30 @@ namespace WPFLocalizeExtension.Extensions
             {
                 if (_resourceBuffer.ContainsKey(key + resourceKey))
                     result = _resourceBuffer[key + resourceKey];
+            }
+
+            if (result == null)
+            {
+                result = LocalizeDictionary.Instance.GetLocalizedObject(resourceKey, null, ci);
+
+                if (result == null)
+                {
+                    var missingKeyEventResult = LocalizeDictionary.Instance.OnNewMissingKeyEvent(this, resourceKey);
+
+                    if (missingKeyEventResult.Reload)
+                        UpdateNewValue();
+
+                    if (LocalizeDictionary.Instance.OutputMissingKeys
+                        && !string.IsNullOrEmpty(_key))
+                    {
+                        if (missingKeyEventResult.MissingKeyResult != null)
+                            result = missingKeyEventResult.MissingKeyResult;
+                        else
+                            result = "Key: " + _key;
+                    }
+                }
                 else
                 {
-                    result = LocalizeDictionary.Instance.GetLocalizedObject(resourceKey, null, ci);
-
-                    if (result == null)
-                        return null;
-
                     key += resourceKey;
                     SafeAddItemToResourceBuffer(key, result);
                 }
